58862daae25e9542354ef893fc2cdf809aa1620c
Harald Steinlechner <haraldsteinlechner@gmail.com>
2017-03-04 12:32:18 +0100

    serial pi

--- Handson/Handson/Handson.cpp
+++ Handson/Handson/Handson.cpp
@@ -12,10 +12,36 @@ void helloWorld()
 	}
 }
 
+double piSerial(long num_steps)
+{
+	int i;
+	double x, pi, sum = 0.0;
+	
+	auto step = 1.0 / (double)num_steps;
+
+	for (i = 0; i < num_steps; i++)
+	{
+		x = (i + 0.5)*step;
+		sum = sum + 4.0 / (1.0 + x*x);
+	}
+	pi = step * sum;
+	return pi;
+}
+
+
+void testPi()
+{
+	double pi, start;
+	start = omp_get_wtime();
+	pi = piSerial(10000000);
+	printf("serial pi: %f in %f\n", pi, omp_get_wtime() - start);
+}
+
 
 int main()
 {
 	//helloWorld();
+	testPi();
     return 0;
 }
 
