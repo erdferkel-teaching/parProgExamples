13db6c53c4f91f7bbb8246b08518c9f0a0b04bb3
Harald Steinlechner <haraldsteinlechner@gmail.com>
2017-03-04 12:09:29 +0100

    hello world

--- Handson/Handson/Handson.cpp
+++ Handson/Handson/Handson.cpp
@@ -1,6 +1,21 @@
+#include <omp.h>
+#include <stdio.h>
+
+void helloWorld()
+{
+	omp_set_num_threads(4);
+#pragma omp parallel
+	{
+		int ID = omp_get_thread_num();
+		printf("hello(%d)", ID);
+		printf(" world(%d)", ID);
+	}
+}
+
 
 int main()
 {
+	//helloWorld();
     return 0;
 }
 
