10e8b1882a2b698407cc493cbf50d87c4518b05f
Harald Steinlechner <haraldsteinlechner@gmail.com>
2017-03-16 08:39:52 +0100

    solution

--- Handson/HandsOnSharp/ParallelForChallenge.cs
+++ Handson/HandsOnSharp/ParallelForChallenge.cs
@@ -66,5 +66,100 @@ namespace HandsOnSharp
             foreach (var thread in threads) thread.Start();
             foreach (var thread in threads) thread.Join();
         }
+
+        public static void MyParallelFor2(int inclusiveLowerBound, int exclusiveUpperBound, Action<int> body)
+        {
+            // Determine the number of iterations to be processed, the number of     
+            // cores to use, and the approximate number of iterations to process in      
+            // each thread.     
+            int size = exclusiveUpperBound - inclusiveLowerBound;
+            int numProcs = Environment.ProcessorCount;
+            int range = size / numProcs;
+
+            // Keep track of the number of threads remaining to complete.     
+            int remaining = numProcs;
+            using (ManualResetEvent mre = new ManualResetEvent(false))
+            {
+                // Create each of the threads.         
+                for (int p = 0; p < numProcs; p++)
+                {
+                    int start = p * range + inclusiveLowerBound;
+                    int end = (p == numProcs - 1) ? exclusiveUpperBound : start + range;
+                    ThreadPool.QueueUserWorkItem(delegate
+                    {
+                        for (int i = start; i < end; i++) body(i);
+                        if (Interlocked.Decrement(ref remaining) == 0) mre.Set();
+                    });
+                }
+                // Wait for all threads to complete.         
+                mre.WaitOne();
+            }
+        }
+
+        public static void MyParallelFor3(int inclusiveLowerBound, int exclusiveUpperBound, Action<int> body)
+        {
+            // Get the number of processors, initialize the number of remaining       
+            // threads, and set the starting point for the iteration.     
+            int numProcs = Environment.ProcessorCount;
+            int remainingWorkItems = numProcs;
+            int nextIteration = inclusiveLowerBound;
+
+            using (ManualResetEvent mre = new ManualResetEvent(false))
+            {
+                // Create each of the work items.        
+                for (int p = 0; p < numProcs; p++)
+                {
+                    ThreadPool.QueueUserWorkItem(delegate
+                    {
+                        int index;
+                        while ((index = Interlocked.Increment(ref nextIteration) - 1) < exclusiveUpperBound)
+                        {
+                            body(index);
+                        }
+                        if (Interlocked.Decrement(ref remainingWorkItems) == 0)
+                            mre.Set();
+                    });
+                }
+
+                // Wait for all threads to complete.         
+                mre.WaitOne();
+            }
+        }
+
+
+        public static void MyParallelFor4(int inclusiveLowerBound, int exclusiveUpperBound, Action<int> body)
+        {
+            // Get the number of processors, initialize the number of remaining      
+            // threads, and set the starting point for the iteration.     
+            int numProcs = Environment.ProcessorCount;
+            int remainingWorkItems = numProcs;
+            int nextIteration = inclusiveLowerBound;
+            const int batchSize = 3;
+
+            using (ManualResetEvent mre = new ManualResetEvent(false))
+            {
+                // Create each of the work items.         
+                for (int p = 0; p < numProcs; p++)
+                {
+                    ThreadPool.QueueUserWorkItem(delegate
+                    {
+                        int index;
+                        while ((index = Interlocked.Add(ref nextIteration, batchSize) - batchSize) < exclusiveUpperBound)
+                        {
+                            // In a real implementation, we’d need to handle                     
+                            // overflow on this arithmetic.                     
+                            int end = index + batchSize;
+                            if (end >= exclusiveUpperBound) end = exclusiveUpperBound;
+                            for (int i = index; i < end; i++) body(i);
+                        }
+                        if (Interlocked.Decrement(ref remainingWorkItems) == 0)
+                            mre.Set();
+                    });
+                }
+
+                // Wait for all threads to complete         
+                mre.WaitOne();
+            }
+        }
     }
 }
