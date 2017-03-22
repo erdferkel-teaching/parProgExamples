2ca9cf866c0f725470646073dde591128ef36700
Harald Steinlechner <haraldsteinlechner@gmail.com>
2017-03-22 10:57:44 +0100

    sync

--- Handson/HandsOnSharp/TaskParallelism.cs
+++ Handson/HandsOnSharp/TaskParallelism.cs
@@ -49,6 +49,75 @@ namespace HandsOnSharp
             WalkSequential(root.Right, action);
         }
 
+        public static void Walk1<T>(Tree<T> root, Action<T> action)
+        {
+            if (root == null) return;
+            Parallel.Invoke(
+                    () => action(root.Data),
+                    () => Walk1(root.Left, action),
+                    () => Walk1(root.Right, action)
+            );
+        }
+
+        public static void Walk2<T>(Tree<T> root, Action<T> action)
+        {
+            if (root == null) return;
+
+            var t1 = Task.Factory.StartNew(() => action(root.Data));
+            var t2 = Task.Factory.StartNew(() => Walk2(root.Left, action));
+            var t3 = Task.Factory.StartNew(() => Walk2(root.Right, action));
+
+            Task.WaitAll(t1, t2, t3);
+        }
+
+        public static void Walk3<T>(Tree<T> root, Action<T> action)
+        {
+            if (root == null) return;
+
+            var t1 = Task.Factory.StartNew(() => action(root.Data), TaskCreationOptions.AttachedToParent);
+            var t2 = Task.Factory.StartNew(() => Walk3(root.Left, action), TaskCreationOptions.AttachedToParent);
+            var t3 = Task.Factory.StartNew(() => Walk3(root.Right, action), TaskCreationOptions.AttachedToParent);
+
+            Task.WaitAll(t1, t2, t3);
+        }
+
+        public static Task Walk4<T>(Tree<T> root, Action<T> action)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                if (root == null) return;
+                action(root.Data);
+                WalkSequential(root.Left, action);
+                WalkSequential(root.Right, action);
+            }, TaskCreationOptions.AttachedToParent);
+        }
+
+        private static Task _completedTask = ((Func<Task>)(() => { var tcs = new TaskCompletionSource<object>(); tcs.SetResult(null); return tcs.Task; }))();
+
+        public static Task Walk5<T>(Tree<T> root, Action<T> action)
+        {
+            if (root == null) return _completedTask;
+            Task t1 = Task.Factory.StartNew(() => action(root.Data));
+            Task<Task> t2 = Task.Factory.StartNew(() => Walk5(root.Left, action));
+            Task<Task> t3 = Task.Factory.StartNew(() => Walk5(root.Right, action));
+            return Task.Factory.ContinueWhenAll(
+                new Task[] { t1, t2.Unwrap(), t3.Unwrap() },
+                tasks => Task.WaitAll(tasks));
+        }
+
+        public static Task ContinueWhenAll(this TaskFactory factory, params Task[] tasks)
+        {
+            return factory.ContinueWhenAll(tasks, completed => Task.WaitAll(completed));
+        }
+
+        public static Task Walk6<T>(Tree<T> root, Action<T> action)
+        {
+            if (root == null) return _completedTask;
+            var t1 = Task.Factory.StartNew(() => action(root.Data));
+            var t2 = Task.Factory.StartNew(() => Walk6(root.Left, action));
+            var t3 = Task.Factory.StartNew(() => Walk6(root.Right, action));
+            return ContinueWhenAll(Task.Factory, t1, t2.Unwrap(), t3.Unwrap());
+        }
 
         public static void Test()
         {
@@ -60,7 +129,35 @@ namespace HandsOnSharp
             sw.Stop();
             Console.WriteLine($"Sequential took: {sw.Elapsed.TotalMilliseconds}");
 
+            sw.Restart();
+            Walk1(t, v => Fib(v));
+            sw.Stop();
+            Console.WriteLine($"Parallel.Invoke took: {sw.Elapsed.TotalMilliseconds}");
 
+            sw.Restart();
+            Walk2(t, v => Fib(v));
+            sw.Stop();
+            Console.WriteLine($"StartNew took: {sw.Elapsed.TotalMilliseconds}");
+
+            sw.Restart();
+            Walk3(t, v => Fib(v));
+            sw.Stop();
+            Console.WriteLine($"StartNew (*3) per level: {sw.Elapsed.TotalMilliseconds}");
+
+            sw.Restart();
+            Walk4(t, v => Fib(v)).Wait();
+            sw.Stop();
+            Console.WriteLine($"One Task per level: {sw.Elapsed.TotalMilliseconds}");
+
+            sw.Restart();
+            Walk5(t, v => Fib(v)).Wait();
+            sw.Stop();
+            Console.WriteLine($"Nested continuation: {sw.Elapsed.TotalMilliseconds}");
+
+            sw.Restart();
+            Walk6(t, v => Fib(v)).Wait();
+            sw.Stop();
+            Console.WriteLine($"Nested continuation 2: {sw.Elapsed.TotalMilliseconds}");
         }
     }
 }
